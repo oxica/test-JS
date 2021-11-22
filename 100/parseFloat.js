d470f7917832399464a0e01949fbf3954dc991f8
oxica <kskaplun@gmail.com>
2021-11-22 23:14:12 +0200

    100

---
+++ 100/parseFloat.js
@@ -0,0 +1,6 @@
+let elementHeight = "200.74px";
+const result = Number.parseFloat(elementHeight);
+console.log(result);
+//200.74
+console.log(typeof result);
+//number
---
+++ 100/parseInt.js
@@ -0,0 +1,6 @@
+let elementWidth = '50px';
+const result = Number.parseInt(elementWidth);
+console.log(result);
+//50
+console.log(typeof result);
+//number
