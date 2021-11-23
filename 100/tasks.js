3f93a339fca9b4f34703853fe4c7d8dd8ad4e986
oxica <kskaplun@gmail.com>
2021-11-23 21:59:00 +0200

    100

---
+++ 100/tasks.js
@@ -0,0 +1,19 @@
+//Напиши скрипт, который просит пользователя ввести число и степень, 
+//возводит число в эту степень и выводит результат в консоль.
+
+let base = prompt("Давай число");
+base = Number(base);
+console.log(base);
+
+let power = prompt("Давай степень");
+power = Number(power);
+console.log(power);
+
+const result = base ** power;
+console.log(result);
+
+//2
+//2
+//4
+__________________________________________
+
