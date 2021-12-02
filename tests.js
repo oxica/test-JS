8b790711f6dec11ba04e03e3ce2275a5cbaf6793
oxica <kskaplun@gmail.com>
2021-12-02 23:25:49 +0200

    tests

---
+++ tests.js
@@ -0,0 +1,114 @@
+
+1. После какого HTML тега нужно вставить JavaScript ?
+ <javascript>
+ <js>
+ <scripting>
+ <script>
+
+2. JavaScript и Java это одно и тоже.
+ Да
+ Нет
+
+3. В каком разделе можно вставить JavaScript?
+ <body>
+ <head> и <body>
+ Нито едно от посочените
+ <head>
+
+4. Как правильно вставить внешний скрипт?
+ <script name="sample1.js">
+ <#include "sample1.js">
+ <script src="sample1.js">
+ <script href="sample1.js">
+
+5. Веншний JavaScript файл должен содержать <script> тег?
+ Да
+ Нет
+
+6. Как можно вывести сообщение "Hello World!" с помощью JavaScript?
+ msg("Hello World");
+ msgBox("Hello World");
+ alertBox("Hello World");
+ alert("Hello World");
+
+7. Как объявить функцию в JavaScript?
+ function:MyFunction()
+ function MyFunction()
+ function = MyFunction()
+ function = New MyFunction()
+
+
+8. Как вызвать функцию "myFunction"?
+ call function myFunction()
+ myFunction()
+ call myFunction()
+
+9. Какой правильный способ использование IF оператора в JavaScript?
+ if i == 2 then
+ if (i == 2)
+ if i = 2
+ if i = 2 then
+
+10. Как можем выполнить какой-либо код, если "i" отличается от 2?
+ if i <> 2
+ if (i != 2)
+ if i != 2 then
+ if (i <> 2)
+
+11. Как начать WHILE цикл?
+ while (i <= 7; i++)
+ while (i <= 7)
+ while i = 1 to 7
+
+12. Как начать FOR цикл?
+ for (i = 0; i <= 10; i++)
+ for (i = 0; i <= 10)
+ for i = 1 to 10
+ for (i <= 10; i++)
+
+13. Какой правильный способ для комментарии в JavaScript?
+ <!-- Это комментарий. -->
+ ' Это комментарий.
+ // Это комментарий.
+
+14. Как сделать комментарий с более чем одной линии?
+ <!-- Этот комментарий имеет более, чем одной линии -->
+ //Этот комментарий имеет более, чем одной линии//
+ /* Этот комментарий имеет более, чем одной линии*/
+
+15. Как создать нового массива в JavaScript?
+ var colors = (1:"yellow", 2:"purple", 3:"blue")
+ var colors = ["yellow", "purple", "blue"]
+ var colors = "yellow", "purple", "blue"
+ var colors = 1 = ("yellow"), 2 = ("purple"), 3 = ("blue")
+
+16. Как округляется число 3.14 до ближайшего целого числа?
+ Math.rnd(3.14)
+ Math.round(3.14)
+ rnd(3.14)
+ round(3.14)
+
+17. Как найти наибольшее из двух чисел?
+ ceil(x, y)
+ Math.ceil(x, y)
+ top(x, y)
+ Math.max(x, y)
+
+18. По какому из превденнъх ниже способах откривается окно с JavaScript?
+ myWindow = window.open("http://www.w3schools.com");
+ myWindow = New window("http://www.w3schools.com");
+ myWindow = window.new("http://www.w3schools.com");
+
+19. Какой из приведенных кодов изменит содержание HTML елемента:
+ <p id="demo">This is a demo text.</p>
+ document.getElementById("demo").innerHTML = "Hello World!";
+ document.getElementByName("p").innerHTML = "Hello World!";
+ $demo.innerHTML = "Hello World!";
+ #demo.text = "Hello World!";
+ document.getElement("p").innerHTML = "Hello World!";
+
+20. Как через JavaScript можем получить имя клиентского браузера?
+ browser.name
+ navigator.appName
+ client.navName
+ 
